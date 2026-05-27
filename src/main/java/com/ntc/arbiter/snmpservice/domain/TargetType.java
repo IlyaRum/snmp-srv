eae53ebbe9a98d5424ed20795aac4f2cf246594a
IlyaRum <tekstir@yandex.ru>
2026-05-27 17:44:41 +0300

    рефакторинг

--- src/main/java/com/ntc/arbiter/snmpservice/domain/TargetType.java
+++ src/main/java/com/ntc/arbiter/snmpservice/domain/TargetType.java
@@ -6,18 +6,6 @@ import static com.ntc.arbiter.snmpservice.agent.SnmpUtils.getResourceString;
  * Тип объекта, с которым произошла проблема
  */
 public enum TargetType {
-    SUPERVISOR {
-        @Override
-        public String toString() {
-            return getResourceString("snmp.target-type.supervisor");
-        }
-    },
-    DATABASE {
-        @Override
-        public String toString() {
-            return getResourceString("snmp.target-type.database");
-        }
-    },
     API {
         @Override
         public String toString() {
@@ -28,12 +16,6 @@ public enum TargetType {
         @Override
         public String toString() {
             return getResourceString("snmp.trap-message.target-type.alarm");
-        }
-    },
-    LDAP {
-        @Override
-        public String toString() {
-            return getResourceString("snmp.target-type.ldap");
         }
     }
 }
